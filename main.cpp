1c73f35af5dad843e201c5e17133b57f137461ee
Aidan <aidanofvt@gmail.com>
2022-11-06 18:11:48 -0500

    Added the ability to pull with a certain force. Algorithm changes.
    Rather than the actuator retreating to a certain position when a command signal is recieved, the actuator now pulls with ~20lbs of force for a certain duration.
    Trials setting a 10kg weight on the actuator showed that the previous algorithm for oscilation-arrest caused a different sort of oscilation in this new setting.
    Major changes to the algorithm were needed to find a compromise between the two scenarios, but although oscillation arrest is now worse, I am now less worried about unexpected extreme oscilation cases.
    It's a bit of a mess, and at this point it's complicated enough that I'm actually not sure which parts of the algorithm are actually productive. More work is needed.

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@
 AnalogIn fromAmp (p20);
 AnalogOut toActuator (p18);
 DigitalIn fromMaster (p19);
-float slickness {0.99}; // An inverted friction value: determines how quickly the actuator stops when no forces are applied
-float inertia {1}; // Determines how reluctantly the actuator accelerates. No real limits to this value.
+float slickness {0.98}; // An inverted friction value: determines how quickly the actuator stops when no forces are applied
+float inertia {0.0006}; // Determines how reluctantly the actuator accelerates. No real limits to this value.
 float inRange {0.3}; // IMPORTANT: this is based on the known range of input voltages. If the voltage range changes, this should change.
 float zeroMagnetism {0.053}; // Based on the known input signal noise.
 float inZero {}; // Based on value at startup
@@ -25,7 +25,7 @@ float inScaled {};
 float outMin {0.0};
 float outMax {1.0};
 float maxSpeed {0.005}; // Per datasheet: max speed 33 inches per second.
-float maxAcceleration {.0004}; // Can fully actuate in ~300ms. Though note that it's accelerating for the first and last ~100ms.
+float maxAcceleration {.0003}; // Can fully actuate in ~300ms. Note, though, that it's accelerating for the first and last ~100ms.
 float velocity{0};
 float deltaV{};
 float command {}; // Allows us more precision in our calculations than AnalogOut allows. Actually does matter.
@@ -54,11 +54,28 @@ float pullToZero (float toRound) {
     else return toRound;
 }
 
+float lerp (int startTime, int endTime, float startValue, float endValue) {
+/*
+"lerp" is a terrible abreviation of "linear interpolation."
+Given two ranges, one of two times, and another of two arbitrary values, returns a value between 
+the two arbitrary values, proportional to the current time on the range between the two times given.
+*/
+    float outPut{(float)(Kernel::get_ms_count() - startTime) / (endTime - startTime) * (endValue - startValue) + startValue};
+    return outPut;
+}
+
 float readInputs () {
 // Updates the 'inScaled' variable, a 0-1 clamped and zero-pulled representation of the force signal.
     inScaledPrior = inScaled;
-    inScaled = pullToZero((clamp(fromAmp, inMin, inMax) - inZero) / inRange);
+    // inScaled = pullToZero((clamp(fromAmp, inMin, inMax) - inZero) / inRange);
+    inScaled = (clamp(fromAmp, inMin, inMax) - inZero) / inRange;
     return inScaled;
+}
+
+float specialSauce (float input) {    
+    float exponent {};
+    exponent = 2.0 + copysign(0.5, input * velocity + 0.00000001);
+    return copysign(pow(abs(input), exponent) / exponent, input);
 }
 
 float calculateFutureAUC () {
@@ -69,13 +86,13 @@ Future values simply assume the current rate of change.
 */
     float slope = inScaled - inScaledPrior;
     float point = inScaled;
-    anticipatedAUC = point + inScaledPrior * 3;
-    for (int i = 4; i > 0; --i) {
+    anticipatedAUC = specialSauce(inScaledPrior) * 3;
+    for (int i = 5; i > 0; --i) {
+        anticipatedAUC += specialSauce(point);
         point += slope;
-        anticipatedAUC += point;
     }
-    // if (slope != 0.0 && Kernel::get_ms_count() % 500 == 0) {
-    //     printf("%f, %f, %f, %f, %f \n", inScaled, inScaledPrior, slope, point, anticipatedAUC);
+    // if (slope != 0.0 && Kernel::get_ms_count() % 1500 == 0) {
+    //     printf("%f, %f, %f, %f, %f \n", inScaled, velocity, slope, point, anticipatedAUC);
     // }
     return anticipatedAUC;
 }
@@ -86,33 +103,32 @@ This is the important part: where the (imaginary/prescriptive) velocity is calcu
 This function is the only content of the main loop, as long as it's not executing a move command.
 If you want to change how the actuator floats, it's probably going to be done here.
 */
-    readInputs();
     calculateFutureAUC();
     // For this formulation, the actuator responds exponentially more to deceleration forces and exponentially less to accelerating forces
-    float exponent = 1.5 + copysign(1.1, anticipatedAUC * velocity + 0.000001);
+    // float exponent = 1.5 + copysign(0.8, anticipatedAUC * velocity + 0.000001);
     // pow() can't handle negative numbers raised to non-integer powers, so we use an ugly workaround
-    float rawDeltaV = copysign(pow(abs(anticipatedAUC) / 64, exponent), anticipatedAUC);
+    // 64 is the theoretical maximum AUC. IPMORTANT: if the AUC formula changes, this magic number sholud change too.
+    // float rawDeltaV = copysign(pow(abs(anticipatedAUC / 64), exponent), anticipatedAUC);
+    float rawDeltaV = anticipatedAUC / 24 * (abs(velocity) + 0.007) * maxAcceleration;
     float deltaV = clamp(rawDeltaV / inertia, -maxAcceleration, maxAcceleration);
     // if (Kernel::get_ms_count() % 200 == 0) {
     //     printf("%f \n", inScaled);
         // printf("%f, %f, %f, %f, %f \n", inScaledPrior, inScaled, velocity, anticipatedAUC, rawDeltaV);
     // }
-    velocity = clamp(velocity * slickness + deltaV, -maxSpeed, maxSpeed);
-    toActuator = command = clamp(command + velocity, outMin, outMax);
-    // If the actuator could have velocity-debt while stuck on the end if its range, that would be bad.
-    if (command >= outMax || command <= outMin) {
-        velocity = 0;
-    }
-}
-
-float lerp (int startTime, int endTime, float startValue, float endValue) {
-/*
-"lerp" is a terrible abreviation of "linear interpolation."
-Given two ranges, one of two times, and another of two arbitrary values, returns a value between 
-the two arbitrary values, proportional to the current time on the range between the two times given.
-*/
-    float outPut{(float)(Kernel::get_ms_count() - startTime) / (endTime - startTime) * (endValue - startValue) + startValue};
-    return outPut;
+    float provisionalVelocity = clamp(velocity * slickness + deltaV, -maxSpeed, maxSpeed);
+    // if (deltaV * velocity < 0) {
+    //     velocity = 0.0;
+    //     inScaled = 0.0;
+    //     ThisThread::sleep_for(40ms);
+    // }
+    // else {
+        velocity = provisionalVelocity;
+        toActuator = command = clamp(command + velocity, outMin, outMax);
+        // If the actuator could have velocity-debt while stuck on the end if its range, that would be bad.
+        if (command >= outMax || command <= outMin) {
+            velocity = 0;
+        }
+    // }
 }
 
 bool move (float to, int duration, bool yield = true) {
@@ -144,6 +160,16 @@ If yield = false, though, the movement will be forced.
     }
 }
 
+void pull (float force, float durationMs) {
+    int endTime{};
+    endTime = Kernel::get_ms_count() + durationMs;
+    while (Kernel::get_ms_count() < endTime) {
+        readInputs();
+        inScaled = clamp(inScaled + (-force), -1.0, 1.0);
+        comply();
+        ThisThread::sleep_for(1ms);
+    }
+}
 
 void calibrate () {
     // This initial delay is to let any physical shaking work itself out before an initial measurement is taken.
@@ -176,17 +202,19 @@ If a user wants to re-define movement limits, or recalibrate input, they are exp
 This will cause the actuator to make some big, abrupt moves though. 
 */
     calibrate();
+    // pull(0.52, 10000000);
     while (true) {
     // If there's a rising edge in the 'retract now' signal, retract.
         if (fromMaster == true && fromMasterPrior == false) {
-            move(outMin, 100, false);
+            pull(0.5, 100);
     //      Following a forced move, wait for the 'retract now' signal to disappear.
             while (fromMaster == true) {
                 ThisThread::sleep_for(1);
             }
         }
-// Otherwise, comply to outside forces.
+    // Otherwise, comply to outside forces.
         else {
+            readInputs();
             comply();
         }
         fromMasterPrior = fromMaster;
